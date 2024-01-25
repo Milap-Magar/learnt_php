1f93ecb77bba6665e6a8f87e6151ddcabbeb3f7d
milap-magar <milapmagar12@gmail.com>
2024-01-25 19:01:32 +0545

    form validation addded

--- form/loksewa.php
+++ form/loksewa.php
@@ -4,24 +4,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LOKSEWA FORM</title>
+    <style>
+        .main-wrapper {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+        }
+
+        .main-form {
+            width: 500px;
+            height: 80vh;
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+        }
+        .sub-wrapper{
+            width: 400px;
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
 </head>
 
 <body>
 
     <?php
-    // define variables to empty values  
-    $nameErr = $emailErr = $mobilenoErr = $genderErr = $websiteErr = $agreeErr = "";
-    $name = $email = $mobileno = $gender = $website = $agree = "";
+    $nameErr = "";
+    $emailErr = "";
+    $mobilenoErr = "";
+    $genderErr = "";
+    $agreeErr = "";
+    $name = "";
+    $email = "";
+    $mobileno = "";
+    $gender = "";
+    $website = "";
+    $agree = "";
 
-    //Input fields validation  
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         //String Validation  
         if (empty($_POST["name"])) {
             $nameErr = "Name is required";
         } else {
-            $name = input_data($_POST["name"]);
+            $name = $_POST["name"];
             // check if name only contains letters and whitespace  
             if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
                 $nameErr = "Only alphabets and white space are allowed";
@@ -32,7 +61,7 @@
         if (empty($_POST["email"])) {
             $emailErr = "Email is required";
         } else {
-            $email = input_data($_POST["email"]);
+            $email = $_POST["email"];
             // check that the e-mail address is well-formed  
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $emailErr = "Invalid email format";
@@ -43,7 +72,7 @@
         if (empty($_POST["mobileno"])) {
             $mobilenoErr = "Mobile no is required";
         } else {
-            $mobileno = input_data($_POST["mobileno"]);
+            $mobileno = $_POST["mobileno"];
             // check if mobile no is well-formed  
             if (!preg_match("/^[0-9]*$/", $mobileno)) {
                 $mobilenoErr = "Only numeric value is allowed.";
@@ -54,89 +83,65 @@
             }
         }
 
-        //URL Validation      
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = input_data($_POST["website"]);
-            // check if URL address syntax is valid  
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-                $websiteErr = "Invalid URL";
-            }
-        }
-
-        //Empty Field Validation  
+        //Gender Field Validation  
         if (empty($_POST["gender"])) {
             $genderErr = "Gender is required";
         } else {
-            $gender = input_data($_POST["gender"]);
+            $gender = $_POST["gender"];
         }
 
         //Checkbox Validation  
         if (!isset($_POST['agree'])) {
             $agreeErr = "Accept terms of services before submit.";
         } else {
-            $agree = input_data($_POST["agree"]);
+            $agree = $_POST["agree"];
         }
-    }
-    function input_data($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
     }
     ?>
 
-    <h2>Registration Form</h2>
-    <span class="error">* required field </span>
-    <br><br>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Name:
-        <input type="text" name="name">
-        <span class="error">*
-            <?php echo $nameErr; ?>
-        </span>
-        <br><br>
-        E-mail:
-        <input type="text" name="email">
-        <span class="error">*
-            <?php echo $emailErr; ?>
-        </span>
-        <br><br>
-        Mobile No:
-        <input type="text" name="mobileno">
-        <span class="error">*
-            <?php echo $mobilenoErr; ?>
-        </span>
-        <br><br>
-        Website:
-        <input type="text" name="website">
-        <span class="error">
-            <?php echo $websiteErr; ?>
-        </span>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" value="male"> Male
-        <input type="radio" name="gender" value="female"> Female
-        <input type="radio" name="gender" value="other"> Other
-        <span class="error">*
-            <?php echo $genderErr; ?>
-        </span>
-        <br><br>
-        Agree to Terms of Service:
-        <input type="checkbox" name="agree">
-        <span class="error">*
-            <?php echo $agreeErr; ?>
-        </span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-        <br><br>
-    </form>
+    <div class="main-wrapper">
+        <h2>Registration Form</h2>
+        <!-- <span class="error">* required field </span> -->
+        <form method="post" action="<?php echo ($_SERVER["PHP_SELF"]); ?>" class="main-form">
+            <div class="sub-wrapper">
+                <label> Name: </label>
+                <input type="text" name="name">
+                <span class="error">*
+                    <?php echo $nameErr; ?>
+                </span>
+            <label> Email: </label>
+            <input type="text" name="email">
+            <span class="error">*
+                <?php echo $emailErr; ?>
+            </span>
+            <label> Mobile No: </label>
+            <input type="text" name="mobileno">
+            <span class="error">*
+                <?php echo $mobilenoErr; ?>
+            </span>
+            <div class="inp-radio">
+                <label> Gender: </label>
+                <input type="radio" name="gender" value="male"> Male
+                <input type="radio" name="gender" value="female"> Female
+                <input type="radio" name="gender" value="other"> Other
+            </div>
+            <span class="error">*
+                <?php echo $genderErr; ?>
+            </span>
+            Agree to Terms of Service:
+            <input type="checkbox" name="agree">
+            <span class="error">*
+                <?php echo $agreeErr; ?>
+            </span>
+            <input type="submit" name="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 
+    <!-- //success case -->
     <?php
     if (isset($_POST['submit'])) {
-        if ($nameErr == "" && $emailErr == "" && $mobilenoErr == "" && $genderErr == "" && $websiteErr == "" && $agreeErr == "") {
+        if ($nameErr == "" && $emailErr == "" && $mobilenoErr == "" && $genderErr == "" && $agreeErr == "") {
             echo "<h3 color = #FF0001> <b>You have sucessfully registered.</b> </h3>";
             echo "<h2>Your Input:</h2>";
             echo "Name: " . $name;
@@ -144,8 +149,6 @@
             echo "Email: " . $email;
             echo "<br>";
             echo "Mobile No: " . $mobileno;
-            echo "<br>";
-            echo "Website: " . $website;
             echo "<br>";
             echo "Gender: " . $gender;
         } else {
