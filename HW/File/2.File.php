90674603c3c7ddb0c88d1634fb0f2839d5771908
milap-magar <milapmagar12@gmail.com>
2024-02-01 10:44:20 +0545

    php learnt

--- HW/File/2.File.php
+++ HW/File/2.File.php
@@ -1,16 +1,17 @@
-//FILE UPLOAD
-    if (isset($_FILES['file'])){
+<?php
+if (isset($_POST["btnSave"])) {
+    $error = [];
+    if (isset($_FILES['file'])) {
         $file = $_FILES['file'];
-        if ($file['error'] == 0){
-            if($file['size'] < 20000000){
-                $filetype = ['application/pdf','application/docx'];
-                if (in_array($file['type'], $filetype )){
-                    $filename = uniqid(). $file['name'] ;
-                    $filesave = move_uploaded_file($file['tmp_name'], 'files/'. $filename);
-                    if ($filesave ){
+        if ($file['error'] == 0) {
+            if ($file['size'] < 20000000) {
+                $filetype = ['application/pdf', 'application/docx'];
+                if (in_array($file['type'], $filetype)) {
+                    $filename = uniqid() . $file['name'];
+                    $filesave = move_uploaded_file($file['tmp_name'], 'files/' . $filename);
+                    if ($filesave) {
                         echo 'File has been Uploaded.';
-                    }
-                    else {
+                    } else {
                         $error['file'] = 'Upload Failed';
                     }
                 } else {
@@ -24,5 +25,38 @@
         }
     } else {
         $error['file'] = 'File Upload Failed';
+    }
 }
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cv Upload</title>
+    <style>
+        .main{
+            display: flex;
+            flex-direction: column;
+        }
+        button{
+            width: 75px;
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data" class="main">
+        <label for="">file uploader</label>
+        <input type="file" name="file" />
+        <span>
+            <?php echo isset($error['file']) ? $error['file'] : ''; ?>
+        </span>
+        <button name="btnSave">Upload</button>
+    </form>
+</body>
+
+</html>
