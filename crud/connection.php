12c1746a860c6ea3549cccb9ebab6a3bf0815836
milap-magar <milapmagar12@gmail.com>
2024-02-04 09:36:40 +0545

    crud removed

--- crud/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    //Set up the database access credentials
-    $hostname = 'localhost'; 
-    $username = 'root'; //your standard uni id
-    $password = '';  // the password found on the W: drive
-    $databaseName = 'C7261148'; //the name of the db you are using on phpMyAdmin
-    $connection = mysqli_connect($hostname, $username, $password, $databaseName) or exit("Unable to connect to database!");
-?>
