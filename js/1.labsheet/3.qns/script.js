1e226f0b36332b3a1f2ca8b2c2ed82a0695d8222
milap-magar <milapmagar12@gmail.com>
2024-02-11 07:02:35 +0545

    qns 4 added

--- js/1.labsheet/3.qns/script.js
+++ js/1.labsheet/3.qns/script.js
@@ -2,5 +2,5 @@ let root = document.getElementById("root");
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
-  root.innerHTML = "This is a created via div.";
+    root.innerHTML = new Date;
 });
