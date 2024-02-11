5ce4f64d9b952a49651196bd98d509bbd647903f
milap-magar <milapmagar12@gmail.com>
2024-02-11 06:57:46 +0545

    Labsheet Added

---
+++ js/1.labsheet/3.qns/script.js
@@ -0,0 +1,6 @@
+let root = document.getElementById("root");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  root.innerHTML = "This is a created via div.";
+});
